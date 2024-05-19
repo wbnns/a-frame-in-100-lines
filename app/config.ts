8c98a098c211687617d013def8f150e200f517a4
wbnns <will.binns@coinbase.com>
2024-05-19 11:26:13 +0200

    refactor(config): Update URL

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
-export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://zizzamia.xyz';
+export const NEXT_PUBLIC_URL = 'https://a-frame-in-100-lines-taupe.vercel.app'
+  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://a-frame-in-10-lines-taupe.vercel.app';
 export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
